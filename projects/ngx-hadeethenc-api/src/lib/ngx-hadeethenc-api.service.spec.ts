53ee6477a955f9a67ebc59a694eb08692cc6ada7
Muhamad Assar <m.assar@outlook.com>
2024-06-15 05:55:57 +0300

    fix demo

--- projects/ngx-hadeethenc-api/src/lib/ngx-hadeethenc-api.service.spec.ts
+++ projects/ngx-hadeethenc-api/src/lib/ngx-hadeethenc-api.service.spec.ts
@@ -2,26 +2,27 @@ import { TestBed } from '@angular/core/testing';
 
 import { NgxHadeethencApiService } from './ngx-hadeethenc-api.service';
 import { provideHttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 describe('NgxHadeethencApiService', () => {
-  let service: NgxHadeethencApiService;
+    let service: NgxHadeethencApiService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [provideHttpClient()],
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [provideHttpClient()],
+        });
+        service = TestBed.inject(NgxHadeethencApiService);
     });
-    service = TestBed.inject(NgxHadeethencApiService);
-  });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-
-  it('#getLanguagesAsync should return real value', async () => {
-    const languages = await service.getLanguagesAsync();
-    expect(languages).toContain({
-      code: 'ar',
-      native: 'عربي',
+    it('should be created', () => {
+        expect(service).toBeTruthy();
     });
-  });
+
+    it('#getLanguagesAsync should return real value', async () => {
+        const languages = await firstValueFrom(service.getLanguages());
+        expect(languages).toContain({
+            code: 'ar',
+            native: 'عربي',
+        });
+    });
 });
