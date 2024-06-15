5951b3f2bac9e352e9c1720e08ca631b80fb2b66
Muhamad Assar <m.assar@outlook.com>
2024-06-15 13:02:55 +0300

    linting

--- projects/ngx-hadeethenc-api/src/lib/ngx-hadeethenc-api.model.ts
+++ projects/ngx-hadeethenc-api/src/lib/ngx-hadeethenc-api.model.ts
@@ -1,60 +1,60 @@
 export interface hadeethsOneResponse {
-  id: string;
-  title: string;
-  /**
-   * matn
-   */
-  hadeeth: string;
-  attribution: string;
-  grade: string;
-  explanation: string;
-  /**
-   * fawaed
-   */
-  hints: string[];
-  categories: any[];
-  translations: string[];
-  words_meanings: wordsMeaning[];
-  reference: string;
-  hadeeth_ar: string;
-  explanation_ar: string;
-  hints_ar: string[];
-  words_meanings_ar: wordsMeaning[];
-  attribution_ar: string;
-  grade_ar: string;
+    id: string;
+    title: string;
+    /**
+     * matn
+     */
+    hadeeth: string;
+    attribution: string;
+    grade: string;
+    explanation: string;
+    /**
+     * fawaed
+     */
+    hints: string[];
+    categories: string[];
+    translations: string[];
+    words_meanings: wordsMeaning[];
+    reference: string;
+    hadeeth_ar: string;
+    explanation_ar: string;
+    hints_ar: string[];
+    words_meanings_ar: wordsMeaning[];
+    attribution_ar: string;
+    grade_ar: string;
 }
 
 export interface wordsMeaning {
-  word: string;
-  meaning: string;
+    word: string;
+    meaning: string;
 }
 
 export interface languageResponse {
-  code: string;
-  native: string;
+    code: string;
+    native: string;
 }
 
 export interface categoriesResponse {
-  id: string;
-  title: string;
-  hadeeths_count: string;
-  parent_id?: string;
+    id: string;
+    title: string;
+    hadeeths_count: string;
+    parent_id?: string;
 }
 
 export interface hadeethsListResponse {
-  data: hadeethsListData[];
-  meta: hadeethsListMeta;
+    data: hadeethsListData[];
+    meta: hadeethsListMeta;
 }
 
 export interface hadeethsListData {
-  id: string;
-  title: string;
-  translations: string[];
+    id: string;
+    title: string;
+    translations: string[];
 }
 
 export interface hadeethsListMeta {
-  current_page: string;
-  last_page: number;
-  total_items: number;
-  per_page: string;
+    current_page: string;
+    last_page: number;
+    total_items: number;
+    per_page: string;
 }
